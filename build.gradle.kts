203e19565c510f6bb58d026b6cc291aded24e466
tharinxd <21027@students.mrgs.school.nz>
2025-07-05 17:01:11 +1200

    deleting google services json file, as new SHA-1 code has been applied on firebase consolse so new josn file has been created and will be added to app files, in replacement of old corrupt file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file
 plugins {
-    id("com.android.application") version "8.8.1" apply false
+    id("com.android.application") version "8.11.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
